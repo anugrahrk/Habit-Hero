31835238b38d04296cf6b51873dd1c0aa8b8e1fe
anugrahrk <chn23mca024@ceconline.edu>
2025-08-07 09:34:51 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className='w-screen h-screen bg-gradient-to-r from-violet-600 to-indigo-600'>
+        
+      </div>
+    </>
+  )
+}
+
+export default App
